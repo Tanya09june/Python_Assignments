4684df143fd8df754e1ab49fbd5f5632c25864c6
Tanya <tanya09june@gmail.com>
2026-03-20 00:19:55 +0200

    2 assignments completed

---
+++ Lecture1/Task2.py
@@ -0,0 +1,19 @@
+#Area=pi*r*2
+PI = 3.14159
+R =input("Enter the Radius of circle = ")
+radius= float(R)
+area = PI*(radius*2)
+print(f"The area of circle is {area:.2f}")
+
+
+#circumference= 2*PI*r
+#radius = input("Enter the radius of circle = ")
+#radius_value = float(radius)
+#circumference = 2*PI*radius_value
+#print(f"The circumference of circle is {circumference:.2f}")
+
+#diameter = 2r, c/r
+#R = input("Enter value of radius = ")
+#radius=float(R)
+#Diameter = 2*radius
+#print (f"Value of Diameter is {Diameter:.2f}")
