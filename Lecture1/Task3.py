b7fabe6529a9c4183d28494963c04ca14f69e40f
Tanya <tanya09june@gmail.com>
2026-03-20 00:37:48 +0200

    3 assignments completed

---
+++ Lecture1/Task3.py
@@ -0,0 +1,21 @@
+#Area = l*w
+#Perimeter = 2(l+w)
+
+L=input("Enter Length of Rectangle = ")
+Length= int(L)
+W=input ("Enter value of Width of Rectangle =")
+Width= int(W)
+Area=Length*Width
+Perimeter= 2*(Length + Width)
+print(f"The area of rectangle is {Area:}")
+print(f"The perimeter of rectangle is {Perimeter:}")
+
+
+#L=input("Enter Length of Rectangle = ")
+#Length= float(L)
+#W=input ("Enter value of Width of Rectangle =")
+#Width= float(W)
+#Area=Length*Width
+#Perimeter= 2*(Length + Width)
+#print(f"The area of rectangle is {Area: .2f}")
+#print(f"The perimeter of rectangle is {Perimeter: .2f}")
