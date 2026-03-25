c74d053d5fe401455a980c0c5d1062c9d31c595e
Tanya <tanya09june@gmail.com>
2026-03-25 13:25:48 +0200

    Final Commit

---
+++ Lecture1/Task6.py
@@ -0,0 +1,9 @@
+import random
+val1 =random.randint(0, 9)
+val2 =random.randint(0,9)
+val3= random.randint(0,9)
+print(f"{val1}{val2}{val3}")
+var1=random.randint(1, 6)
+var2= random.randint(1 ,6)
+var3= random.randint (1, 6)
+print(f"{var1}{var2}{var3}")
