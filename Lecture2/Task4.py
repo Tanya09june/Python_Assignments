4dc138371abff0cbbe1d18964fd4b275d8d46337
Tanya <tanya09june@gmail.com>
2026-03-27 18:28:21 +0200

    Final Commit

--- Lecture2/Task4.py
+++ Lecture2/Task4.py
@@ -6,7 +6,7 @@
 
 
 year=int(input("Enter the Year:- "))
-if (year % 4 == 0 or year % 400 == 0) and (year % 100 != 0):
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print(f" {year:} is a Leap Year!")
 else:
     print(f" {year:} is not a leap year")
