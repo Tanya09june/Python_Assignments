556fec408b09c1acd55efa0042ebffbd7deee595
Tanya <tanya09june@gmail.com>
2026-03-31 17:25:11 +0300

    Final Commit

--- Lecture2/Task1.py
+++ Lecture2/Task1.py
@@ -3,6 +3,6 @@ Length = float(input("Enter the length of zander in cm:- "))
 if Length >= LIMIT:
     print (f"Good Job! Trapped")
 else:
-    Difference= LIMIT - Length
-    print(f"The zander is below the legal size limit. \nPlease release fish back into the lake. \nIts {Difference:.2f} CM short of limit.")
+    difference= LIMIT - Length
+    print(f"The zander is below the legal size limit. \nPlease release fish back into the lake. \nIts {difference:.2f} CM short of limit.")
 
